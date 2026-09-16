fc90c5d156f998c98e0409bc855aea105b732a8c
stevenmarfe-lab <stevenmarfe@gmail.com>
2026-09-16 02:03:14 +0000

    ajustes Demo

--- Demo.java
+++ Demo.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class Demo {
 
+public static void main (String[] args){
+   
+   int edad =25;
+   double habilidad;
+   String nombre;
+   boolean esta_lesionado;
+   String equipo;
+   int estatura;
 
+   // edad=21; se puede dar el valor aqui o arriba
+   habilidad=8.5;
+   nombre= "Brian Martinez";
+   esta_lesionado= false;  // true o false
+   equipo= "Madrid";
+   estatura=178;
 
-    
+System.out.println("Informacion del jugador: ");
+System.out.println("Nombre: " + nombre);
+System.out.println("Edad: " + edad);
+System.out.println("Habilidad: " + habilidad);
+System.out.println("Estatura: " + estatura);
+System.out.println("Esta lesionado: " + esta_lesionado);
+
+}
 }
